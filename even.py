fa8988dd7ea8bd7767bf3e4a9b919d967518bca4
vasumasini3174 <vasumasini17@gmail.com>
2024-11-28 10:43:23 +0530

    Update branch1.py

--- even.py
+++ even.py
@@ -4,6 +4,6 @@ b=3;
 if( a%2==0 and b%2==0):
     print("Both ",a," and ",b," are even numbers.");
 elif( a%2==0 ):
-    print("Only ",a," is an even number.");
+    print("only a is an even number");
 else:
     print("Only ",b," is an even number")
