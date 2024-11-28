790eab79a6411491d0b83dcb2abecb57342378b5
Vasu_masini <vasumasini17@gmail.com>
2024-11-28 10:33:37 +0530

    first Commit for even numbers

---
+++ even.py
@@ -0,0 +1,9 @@
+a=4;
+b=3;
+
+if( a%2==0 and b%2==0):
+    print("Both ",a," and ",b," are even numbers.");
+elif( a%2==0 ):
+    print("Only ",a," is an even number.");
+else:
+    print("Only ",b," is an even number")
